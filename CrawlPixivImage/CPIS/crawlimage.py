85fedecc8eb119b9e772107aa74b49166adea54c
DarkChii <906835882@qq.com>
2018-01-28 01:24:09 +0800

    Add files via upload

---
+++ CrawlPixivImage/CPIS/crawlimage.py
@@ -0,0 +1,123 @@
+import requests
+from tqdm import tqdm
+import re
+
+class CrawlerPixivImg(object):
+    def __init__(self, url, params, headers):
+        object.__init__(self)
+        self.url = url
+        self.params = params
+        self.headers = headers
+
+    def GetSmallImgUrl(self):   # 解决异步加载机制
+        r = requests.get(self.url, params=self.params, headers=self.headers)
+        # print(r.url)
+        Elem = str(r.json()['contents'])    # 自动处理json文件
+        recmp = re.compile(r'\'url\': \'(.+?.jpg)\'')   # 从json文件中获取所有小图片的url存放到列表中
+        SmallImgUrlList = recmp.findall(Elem)
+        return SmallImgUrlList
+
+    def GetMediumImgUrl(self, urlList):# https://i.pximg.net/c/600x600/img-master/img/2018/01/17/13/33/41/66832795_p0_master1200.jpg
+        MediumImgUrlList = []
+        for url in urlList:
+            MediumImgUrlList.append(url.replace('/240x480/', '/600x600/'))
+        return MediumImgUrlList
+
+    def GetLargestImgUrl(self, urlList):# https://i.pximg.net/img-original/img/2018/01/17/13/33/41/66832795_p0.jpg
+        LargestImgUrlList = []
+        for url in urlList:
+            LargestImgUrl = url.replace('/c/240x480/img-master/','/img-original/')
+            LargestImgUrlList.append(LargestImgUrl.replace('_master1200',''))
+        return LargestImgUrlList
+
+    def DownloadProcess(self, url, IMGLibrary = {}):
+        ImgName = url.split('/')[-1]  # [-1]的意思是从网址中最后一个'/'的后一位开始匹配 获取文件名
+        ImgID = ImgName[:8]
+        headers = {
+            'Referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + ImgID,  # Pixiv反爬虫机制
+            'User-Agent': 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+        }
+        status = requests.get(url, headers=headers, stream=True).status_code
+        chunk_size = 1024
+
+        if status != (404 or 403):
+            if ImgName in IMGLibrary:   # 如果已经下载图片 则退出函数
+                print('The same picture exists!')
+                return
+
+            with open('Pixiv_Img/url.txt', 'a') as fp:
+                fp.write(ImgName + ':')
+
+            with open('Pixiv_Img/url.txt', 'a') as fp:
+                fp.write(str(url) + '\r\n')
+
+            imgsize = requests.get(url, headers=headers).headers['content-length']
+
+            # 显示下载进度条
+            ProgressBar = tqdm(
+                total=int(imgsize),
+                initial=0,
+                unit='B',
+                unit_scale=True,
+                desc=ImgName
+            )
+
+            with requests.get(url, headers=headers, stream=True) as img:
+                with open('Pixiv_Img/{}.jpg'.format(ImgID), 'wb') as fp:
+                    for chunk in img.iter_content(chunk_size=chunk_size):
+                        if chunk:
+                            fp.write(chunk)
+                            ProgressBar.update(chunk_size)
+                    ProgressBar.close()
+
+            dict = {ImgName: ImgID}
+            IMGLibrary.update(dict)
+
+        if status == (404 or 403):  # 如果jpg格式图片错误 则用png格式保存
+            url = url.replace('jpg','png')
+
+            status = requests.get(url, headers=headers, stream=True).status_code
+            if status == (404 or 403):  # 如果图片格式不是jpg 或 png 则退出
+                print('图片格式不支持下载或者链接被禁止访问!')
+                return
+
+            ImgName = url.split('/')[-1]  # [-1]的意思是从网址中最后一个'/'后一位开始匹配 获取文件名
+            ImgID = ImgName[0:8]    # 截取图片ID
+            headers = {
+                'Referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + ImgID,  # Pixiv反爬虫机制
+                'User-Agent': 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+            }
+
+            if ImgName in IMGLibrary:  # 如果已经下载图片 则退出函数
+                print('The same picture exists!')
+                return
+
+            with open('Pixiv_Img/url.txt', 'a') as fp:
+                fp.write(ImgName + ':')
+
+            with open('Pixiv_Img/url.txt','a') as fp:
+                fp.write(str(url) + '\r\n')
+
+            imgsize = requests.get(url, headers=headers).headers['content-length']
+
+            ProgressBar = tqdm(
+                total=int(imgsize),
+                initial=0,
+                unit='B',
+                unit_scale=True,
+                desc=ImgName
+            )
+
+            with requests.get(url, headers=headers, stream=True) as img:
+                with open('Pixiv_Img/{}.png' .format(ImgID), 'wb') as fp:
+                    for chunk in img.iter_content(chunk_size=chunk_size):
+                        if chunk:
+                            fp.write(chunk)
+                            ProgressBar.update(chunk_size)
+                    ProgressBar.close()
+
+            dict = {ImgName:ImgID}
+            IMGLibrary.update(dict)
+
+        else:
+            return
---
+++ CrawlPixivImage/example.py
@@ -0,0 +1,58 @@
+'''
+@Author        : darkchii
+@Create date   : 2018-01-21
+@Theme         : WebCrawler - CrawlPixivImage
+'''
+from CPIS.crawlimage import crawlimage
+from multiprocessing import Pool
+import time
+import sys
+
+def example():
+    try:
+        with open('Pixiv_Img', 'r'):
+            pass
+    except FileNotFoundError:
+        print('请在该程序目录下创建一个\'Pixiv_Img\'文件夹后再运行!')
+        sys.exit(1)
+    except PermissionError:
+        pass
+    try:
+        with open('Pixiv_Img/url.txt', 'a'):
+            pass
+    except FileNotFoundError:
+        with open('Pixiv_Img/url.txt', 'w'):
+            pass
+
+    IMGLibrary = {}
+    init_url = 'https://www.pixiv.net/ranking.php?mode=daily'
+    for date in range(20170125,20170128):   # 排行榜日期
+        for p in range(1,6):   # 异步加载的页面大概有几十页 每一页共50个小图片链接 这里随意测试 而且排行榜中会有前几天甚至前几百天的图片 所以会有许多重复的图片 因此实际上没必要把一张排行榜的全部爬完
+            # 异步加载 url格式：https://www.pixiv.net/ranking.php?mode=daily&date=日期&p=页码&format=json&tt=6a124b46e04507dcee2efed9bac25cf5
+            params = {
+                'date':'{}'.format(str(date)),
+                'p':'{}'.format(str(p)),
+                'format':'json',
+                'tt':'6a124b46e04507dcee2efed9bac25cf5'
+            }
+            headers = {
+                'Referer': 'https://www.pixiv.net/ranking.php?mode=daily&date={}'.format(str(date)),
+                'User-Agent': 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+            }
+            Page = CrawlerPixivImg(init_url, params=params, headers=headers)    # 初始化
+            SmallImgUrlList = Page.GetSmallImgUrl()
+            PixivImgUrlList = Page.GetLargestImgUrl(SmallImgUrlList)
+
+            Process = Pool(6)   # 开启6个进程并行下载 一般默开启电脑CPU核心个数 这里用多进程下载比多线程快很多
+            for url in PixivImgUrlList:
+                Process.apply_async(Page.DownloadProcess, args=(url, IMGLibrary))
+            Process.close()
+            Process.join()
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    start = time.time()
+    example()
+    end = time.time()
+    print('下载用时：',float(end - start))
