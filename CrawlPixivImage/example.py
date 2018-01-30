4c78d1e3c0517ba808f7b9f8babd91f0b3bb24f2
DarkChii <906835882@qq.com>
2018-01-30 23:53:14 +0800

    Update example.py

--- CrawlPixivImage/example.py
+++ CrawlPixivImage/example.py
@@ -11,23 +11,13 @@ import os
 
 
 def example():
-    if not os.path.exists('Pixiv_Img'):
-        print('请在该程序目录下创建一个\'Pixiv_Img\'文件夹后再运行该程序!')
-        os._exit(1)
-
-    try:
-        with open('Pixiv_Img/url.txt', 'a'):
-            pass
-    except FileNotFoundError:
-        with open('Pixiv_Img/url.txt', 'w'):
-            pass
 
     img_library = {}
     init_url = 'https://www.pixiv.net/ranking.php?mode=daily'
     for date in range(20180128,20180130):   # 排行榜日期
         # bug update:如果日期为当天排行榜 则 url 格式为 https://www.pixiv.net/ranking.php?mode=daily&p=页码&format=json&tt=6a124b46e04507dcee2efed9bac25cf5
-        for p in range(1,4):   # 异步加载的页面大概有几十页 每一页共50个小图片链接 这里随意测试 而且排行榜中会有前几天甚至前几百天的图片 所以会有许多重复的图片 因此实际上没必要把一张排行榜的全部爬完
-            # url格式：https://www.pixiv.net/ranking.php?mode=daily&date=日期&p=页码&format=json&tt=6a124b46e04507dcee2efed9bac25cf5
+        for p in range(1,4):
+            # https://www.pixiv.net/ranking.php?mode=daily&date=日期&p=页码&format=json&tt=6a124b46e04507dcee2efed9bac25cf5
             params = {
                 'date':str(date),
                 'p':str(p),
@@ -59,4 +49,4 @@ if __name__ == '__main__':
     start = time.time()
     example()
     end = time.time()
-    print('此次下载用时:{} min'.format(float(end - start)/60))
+    print('此次下载用时:{} min'.format((end - start)/60))
