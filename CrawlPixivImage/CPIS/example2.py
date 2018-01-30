e70a0149ea6b2859eaad6e8fc255ba58960859e3
DarkChii <906835882@qq.com>
2018-01-30 23:28:57 +0800

    example2.py

---
+++ CrawlPixivImage/CPIS/example2.py
@@ -0,0 +1,13 @@
+from CPIS.crawlpixiv import CrawlProcess
+
+def main():
+    user_agent = 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+    img_library = {}
+    
+    CrawlProcess(user_agent=user_agent,img_library=img_library).run()
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    print('下载用时：{} min'.format((end - start)/60))
