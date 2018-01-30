b7b14c58e75aca7f5c392b0fbfbda742a6c34236
DarkChii <906835882@qq.com>
2018-01-30 23:47:30 +0800

    Update example2.py

--- CrawlPixivImage/CPIS/example2.py
+++ CrawlPixivImage/CPIS/example2.py
@@ -1,4 +1,5 @@
 from CPIS.crawlpixiv import CrawlProcess
+import time
 
 def main():
     user_agent = 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
