6d2d4e459fe6e23956135b0b541af6b1b6986fb4
DarkChii <906835882@qq.com>
2018-01-31 00:30:56 +0800

    Update crawlpixiv.py

--- CrawlPixivImage/CPIS/crawlpixiv.py
+++ CrawlPixivImage/CPIS/crawlpixiv.py
@@ -4,6 +4,8 @@ import re,os,time
 import requests
 
 class CrawlerPixivImg(object):
+    __chunk_size = 1024
+
     def __init__(self, url, params, headers):
         object.__init__(self)
         self.url = url
@@ -13,7 +15,7 @@ class CrawlerPixivImg(object):
     def get_small_img_url(self):
         r = requests.get(self.url, params=self.params, headers=self.headers, timeout=1)
         # print(r.url)
-        json_file = r.json()['contents']    # 自动处理json文件
+        json_file = r.json()['contents']    # 处理json文件
         cut_url = re.compile(r'\'url\': \'(.+?.jpg)\'')
         small_img_url_list = cut_url.findall(str(json_file))
         return small_img_url_list
@@ -31,20 +33,38 @@ class CrawlerPixivImg(object):
             largest_img_url_list.append(largest_img_url.replace('_master1200',''))
         return largest_img_url_list
 
+    def __show_progress_bar(self,url,headers,img_name):
+        img_size = requests.get(url, headers=headers, timeout=1).headers['content-length']
+
+        progress_bar = tqdm(
+            total=int(img_size),
+            initial=0,
+            unit='B',
+            unit_scale=True,
+            desc=img_name
+        )
+
+        with requests.get(url, headers=headers, stream=True) as img:
+            with open('Pixiv_Img/{}'.format(img_name), 'wb') as fp:
+                for chunk in img.iter_content(chunk_size=self.__chunk_size):
+                    if chunk:
+                        fp.write(chunk)
+                        progress_bar.update(self.__chunk_size)
+                progress_bar.close()
+
     def download_pixiv_img(self, url, img_name_library = {}):
-        img_name = url.split('/')[-1]  # [-1]的意思是从网址中最后一个'/'的后一位开始匹配 获取文件名
+        img_name = url.split('/')[-1]
         img_id = img_name[:8]
 
         headers = {
-            'Referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + img_id,  # Pixiv反爬虫机制
+            'Referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + img_id,
             'User-Agent': 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
         }
-
-        chunk_size = 1024
 
         response_status = requests.get(url, headers=headers, timeout=1).status_code
 
         if response_status != (404 or 403):
+
             if img_name in img_name_library:   # 如果已经下载图片 则退出函数
                 print('The same picture exists!')
                 return
@@ -55,24 +75,7 @@ class CrawlerPixivImg(object):
             with open('Pixiv_Img/url.txt', 'a') as fp:
                 fp.write(str(url) + '\r\n')
 
-            img_size = requests.get(url, headers=headers, timeout=1).headers['content-length']
-
-            # 显示下载进度条
-            progress_bar = tqdm(
-                total=int(img_size),
-                initial=0,
-                unit='B',
-                unit_scale=True,
-                desc=img_name
-            )
-
-            with requests.get(url, headers=headers, stream=True) as img:
-                with open('Pixiv_Img/{}.jpg'.format(img_id), 'wb') as fp:
-                    for chunk in img.iter_content(chunk_size=chunk_size):
-                        if chunk:
-                            fp.write(chunk)
-                            progress_bar.update(chunk_size)
-                    progress_bar.close()
+            self.__show_progress_bar(url,headers,img_name)
 
             dict = {img_name: img_id}
             img_name_library.update(dict)
@@ -85,14 +88,14 @@ class CrawlerPixivImg(object):
                 print('图片格式不支持下载或者链接被禁止访问!')
                 return
 
-            img_name = url.split('/')[-1]  # [-1]的意思是从网址中最后一个'/'后一位开始匹配 获取文件名
-            img_id = img_name[:8]    # 截取图片ID
+            img_name = url.split('/')[-1]
+            img_id = img_name[:8]
             headers = {
-                'Referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + img_id,  # Pixiv反爬虫机制
+                'Referer': 'https://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + img_id,
                 'User-Agent': 'Mozilla/5.0(Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
             }
 
-            if img_name in img_name_library:  # 如果已经下载图片 则退出函数
+            if img_name in img_name_library:
                 print('The same picture exists!')
                 return
 
@@ -102,33 +105,15 @@ class CrawlerPixivImg(object):
             with open('Pixiv_Img/url.txt','a') as fp:
                 fp.write(str(url) + '\r\n')
 
-            img_size = requests.get(url, headers=headers, timeout=1).headers['content-length']
-
-            progress_bar = tqdm(
-                total=int(img_size),
-                initial=0,
-                unit='B',
-                unit_scale=True,
-                desc=img_name
-            )
-
-            with requests.get(url, headers=headers, stream=True) as img:
-                with open('Pixiv_Img/{}.png' .format(img_id), 'wb') as fp:
-                    for chunk in img.iter_content(chunk_size=chunk_size):
-                        if chunk:
-                            fp.write(chunk)
-                            progress_bar.update(chunk_size)
-                    progress_bar.close()
+            self.__show_progress_bar(url,headers,img_name)
 
             dict = {img_name:img_id}
             img_name_library.update(dict)
-
-        else:
-            return
+            
 
 class CrawlProcess(object):
     __init_url = 'https://www.pixiv.net/ranking.php?mode=daily'
-    
+
     def __init__(self,start_date=None,end_date=None,end_page=None,user_agent='',img_library = {}):
         object.__init__(self)
         if not os.path.exists('Pixiv_Img'):
